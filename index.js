7fc60e857f751bb40f31018f9d476f01d592ec41
Spencer Krum <nibz@spencerkrum.com>
2020-08-31 19:04:39 +0000

    Template out featured artists list

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ fastify.register(require('point-of-view'), {
   }
 })
 
+var artist1 = {
+    'id': '101010101',
+    'name': 'The Big Tones',
+    'genre': 'Soul',
+    'active': false,
+    'hometown': 'Penn Station, NY',
+}
+var artist2 = {
+    'id': '1838481194',
+    'name': 'Flipper',
+    'genre': 'Rock',
+    'active': true,
+    'hometown': 'Eugene, OR',
+}
+
+
+// var db = connect(db://something)
+var artists = [];
+artists.push(artist1)
+artists.push(artist2)
+
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
@@ -16,6 +37,12 @@ fastify.get('/', async (request, reply) => {
 fastify.get('/test', async (req, reply) => {
 //  const t = await something()
     return reply.view('/templates/test.njk', { title: 'hello world', text: 'text', items: {"sally": "dog", "mike": "cat", "peanut": "ferret" }})
+})
+
+// With async handler be sure to return the result of reply.view
+fastify.get('/artists', async (req, reply) => {
+//  const t = await something()
+    return reply.view('/templates/artists.njk', { title: 'Featured Artists', artists })
 })
 
 const start = async () => {
